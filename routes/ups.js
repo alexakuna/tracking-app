f94792a76eb4495cd12b689de7defc69dc81a575
mac <alexxjc@gmail.com>
2021-03-26 15:14:12 +0200

    update to 0.1.3 vesion / 2

--- routes/ups.js
+++
@@ -1,9 +0,0 @@
-
-
-// document.querySelectorAll('.ups-data_table')[0]
-//     .querySelector('#stApp_ShpmtProg_LVP_milestone_1_date_1')
-// URL - https://www.ups.com/track?loc=ru_UA&tracknum=<here-track-number>&requester=WT/trackdetails
-
-// 1. Получение данных с АПИ ВестернБид
-// 2. Вывод данных на страницу
-// 3. Отслеживание посылок
